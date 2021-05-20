ca9f224b7378cd4d66abfca2a907cb3b4ba0db47
Jim Gongora <jim.gongora@gmail.com>
2021-05-20 22:25:38 +0800

    My first commit! 😀

---
+++ main.php
@@ -0,0 +1,59 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+    <title>Power Plant Mall Electrical</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+
+<script>
+function t308() {
+  alert("You clicked the coffee bean!");
+}
+</script>
+
+
+</head>
+
+<body >
+<div id="wrapper">
+  <img src="img/R3.svg" />
+<a class="image" title="301" style="position: absolute; left: 10.17%; top: 48.75%; width: 12.92%; height: 10.5%; z-index: 2;"><div class="popup" onclick="myFunction()">301 - Fully Booked
+  <span class="popuptext" id="myPopup" >A Simple Popup!</span>
+</div></a>
+<a class="image" title="302" style="position: absolute; left: 25%; top: 51.63%; width: 2%; height: 5.13%; z-index: 2;"></a>
+<a class="image" title="303" style="position: absolute; left: 23.17%; top: 49%; width: 3.83%; height: 2.5%; z-index: 2;"></a>
+<a class="image" title="304" style="position: absolute; left: 23.08%; top: 45.5%; width: 3.83%; height: 3.5%; z-index: 2;"></a>
+<a class="image" title="306" style="position: absolute; left: 32.83%; top: 48.75%; width: 7.08%; height: 4%; z-index: 2;"></a>
+<a class="image" title="308" style="position: absolute; left: 32.83%; top: 52.88%; width: 3.67%; height: 6.75%; z-index: 2;"></a>
+<a class="image" title="309a" style="position: absolute; left: 36.58%; top: 52.88%; width: 3.33%; height: 6.75%; z-index: 2;"></a>
+<a class="image" title="309b" style="position: absolute; left: 39.92%; top: 54.63%; width: 1.67%; height: 5%; z-index: 2;"></a>
+<a class="image" title="309c" style="position: absolute; left: 41.58%; top: 54.63%; width: 1.67%; height: 5%; z-index: 2;"></a>
+<a class="image" title="309d" style="position: absolute; left: 43.25%; top: 56%; width: 3.25%; height: 3.63%; z-index: 2;"></a>
+<a class="image" title="310" style="position: absolute; left: 46.5%; top: 57.13%; width: 4.42%; height: 2.5%; z-index: 2;"></a>
+
+  
+</div>
+
+
+
+<script>
+// When the user clicks on div, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
+
+</body>
+
+</html>
